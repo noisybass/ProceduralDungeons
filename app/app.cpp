a06c831c2b7667309af4e5ae25b21daea0824d4c
NoisyBass <laura.dcs4@gmail.com>
2017-06-28 19:38:49 +0200

    Added SFML for better visualization

--- app/app.cpp
+++ app/app.cpp
@@ -1,22 +1,79 @@
 #include "caveGenerator\caveGenerator.h"
+#include "SFML\Graphics.hpp"
 
 #include <iostream>
+#include <vector>
 
 int main(int argc, char **argv)
 {
-	ProceduralCaves::CaveGenerator generator{20, 60, 50, 3};
+	unsigned int mapWidth = 80;
+	unsigned int mapHeight = 80;
+	int cellSize = 10;
+
+	sf::RenderWindow window(sf::VideoMode(mapWidth*cellSize, mapHeight*cellSize), "Procedural Map Generator");
+
+	std::vector<std::vector<sf::CircleShape>> graphicMap;
+
+	sf::CircleShape square(cellSize, 4);
+	square.setOrigin(square.getRadius(), square.getRadius());
+	square.setRotation(45.0f);
+	for (int i = 0; i < mapWidth; ++i)
+	{
+		graphicMap.push_back(std::vector<sf::CircleShape>());
+		for (int j = 0; j < mapHeight; ++j)
+		{
+			graphicMap[i].push_back(square);
+		}
+	}
+
+	ProceduralCaves::CaveGenerator generator{mapWidth, mapHeight, 50, 3};
 
 	std::vector<std::vector<int>> map = generator.GenerateMap();
 
-	for (std::vector<int> row : map)
+	// Print the map
+	//for (std::vector<int> row : map)
+	//{
+	//	for (int cell : row)
+	//	{
+	//		std::cout << ((cell == 1) ? "#" : " ");
+	//	}
+
+	//	std::cout << std::endl;
+	//}
+
+	while (window.isOpen())
 	{
-		for (int cell : row)
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
 		{
-			std::cout << ((cell == 1) ? "#" : " ");
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if (event.type == sf::Event::MouseButtonPressed)
+			{
+				map = generator.GenerateMap();
+			}
 		}
 
-		std::cout << std::endl;
-	}
+		window.clear();
 
-	std::cout << "stop" << std::endl;
+		for (int i = 0; i < mapWidth; ++i)
+		{
+			for (int j = 0; j < mapHeight; ++j)
+			{
+				graphicMap[i][j].setPosition(i*cellSize + square.getRadius() - 2.0f, j*cellSize + square.getRadius() - 2.0f);
+
+				if (map[i][j] == 1)
+					graphicMap[i][j].setFillColor(sf::Color(69, 115, 133));
+				else
+					graphicMap[i][j].setFillColor(sf::Color(35, 35, 35));
+
+				window.draw(graphicMap[i][j]);
+			}
+		}
+
+		window.display();
+
+	}
 }
